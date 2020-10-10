1ba50ed6bcfd0842aabd8d080f17bada30eb9e9f
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2020-10-10 14:09:28 -0300

    Create config_db.php

---
+++ config_db.php
@@ -0,0 +1,27 @@
+<?php
+$servidor = "localhost";
+$usuario = "adriana_santos";
+$senha = "147852";
+$banco = "adriana_agenda_db";
+
+/*
+include 'config_db.php';
+
+$hora = 23;
+	
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+	
+$sql = "INSERT INTO config_semana(hora) VALUES ('$hora')";
+
+$yes = "Registro Adicionado!!";
+$no = "Erro ao adicionar!!";	
+	
+if ($conn->query($sql) === True) {
+	echo "<script type='text/javascript'>alert('$yes');</script>";
+} else {
+	echo "<script type='text/javascript'>alert('$no');</script>";
+}
+
+$conn->close();
+*/
+?>
